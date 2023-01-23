91de20c7de3370881c6eca8c770cd9ad5dbce740
Adrian <adrian.korzan@gmail.com>
2023-01-23 18:13:01 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import DartTable from './components/DartTable.vue'
+</script>
+
+<template>
+  <DartTable />
+</template>
+
+<style lang="sass" scoped>
+</style>
