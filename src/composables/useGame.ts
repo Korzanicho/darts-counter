1d546147293adc593da876127ebcaf519d63c518
Adrian <adrian.korzan@gmail.com>
2023-02-16 01:00:40 +0100

    [27] ts refactor

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -1,64 +1,65 @@
 import { Ref, computed, ref } from 'vue'
-import type { Player } from '../interfaces'
+import type { ComputedRef } from 'vue'
+import type { Player, UseGame } from '../interfaces'
 import { usePlayers } from './usePlayers'
 import { useLocalstorage } from './useLocalstorage';
 
 const localStorage = useLocalstorage();
 
-const states = [
+const states: string[] = [
 	'before',
 	'playing',
 	'after'
 ]
 
-const state = ref(states[0]);
-const currentPlayerId = ref<number>(0);
-const currentTurn = ref(0);
-const darts = ref(3);
-const currentWinnerName = ref('');
+const state: Ref<string> = ref(states[0]);
+const currentPlayerId: Ref<number> = ref(0);
+const currentTurn: Ref<number> = ref(0);
+const darts: Ref<number> = ref(3);
+const currentWinnerName: Ref<string> = ref('');
 const winners: Ref<Player[]> = ref([]);
 
-export const useGame = () => {
+export const useGame: Function = (): UseGame => {
 
-  const addWinner = (player: Player) => {
+  const addWinner = (player: Player): void => {
     winners.value.push(player);
     localStorage.saveWinners(winners.value);
   }
   
-  const setWinners = (newWinners: Player[]) => {
+  const setWinners = (newWinners: Player[]): void => {
     winners.value = newWinners;
     localStorage.saveWinners(newWinners);
   };
 
-  const setState = (newState: string) => {
+  const setState = (newState: string): void => {
     state.value = newState;
     localStorage.saveState(newState);
   };
 
-  const setCurrentPlayerId = (id: number) => {
+  const setCurrentPlayerId = (id: number): void => {
     currentPlayerId.value = id;
     localStorage.saveCurrentPlayerId(id);
   };
 
-  const setCurrentTurn = (newTurn: number) => {
+  const setCurrentTurn = (newTurn: number): void => {
     currentTurn.value = newTurn;
     localStorage.saveCurrentTurn(newTurn);
   };
 
-  const setDarts = (newDarts: number) => {
+  const setDarts = (newDarts: number): void => {
     darts.value = newDarts;
   };
 
-  const setCurrentWinnerName = (name: string) => {
+  const setCurrentWinnerName = (name: string): void => {
     currentWinnerName.value = name;
   };
 
-  const changePlayerToNext = () => {
+  const changePlayerToNext = (): void => {
     let players: Player[] = usePlayers().getPlayers.value;
     players = players.filter(player => player.score != 0);
 
-    const currentPlayerIndex = players.findIndex(player => player.id === currentPlayerId.value);
-    const nextPlayerIndex = currentPlayerIndex + 1;
+    const currentPlayerIndex: number = players.findIndex(player => player.id === currentPlayerId.value);
+    const nextPlayerIndex: number = currentPlayerIndex + 1;
     if (nextPlayerIndex >= players.length) {
       setCurrentPlayerId(players[0].id);
       setCurrentTurn(getCurrentTurn.value + 1);
@@ -67,7 +68,7 @@ export const useGame = () => {
     }
   };
 
-  const resetGame = () => {
+  const resetGame = (): void => {
     setState(states[0]);
     setCurrentPlayerId(0);
     setCurrentTurn(0);
@@ -78,12 +79,12 @@ export const useGame = () => {
   };
 
 
-  const getState = computed(() => state.value);
-  const getDarts = computed(() => darts.value);
-  const getWinners = computed(() => winners.value);
-  const getCurrentTurn = computed(() => currentTurn.value);
-  const getCurrentPlayerId = computed(() => currentPlayerId.value);
-  const getCurrentWinnerName = computed(() => currentWinnerName.value);
+  const getState: ComputedRef<string> = computed(() => state.value);
+  const getDarts: ComputedRef<number> = computed(() => darts.value);
+  const getWinners: ComputedRef<Player[]> = computed(() => winners.value);
+  const getCurrentTurn: ComputedRef<number> = computed(() => currentTurn.value);
+  const getCurrentPlayerId: ComputedRef<number> = computed(() => currentPlayerId.value);
+  const getCurrentWinnerName: ComputedRef<string> = computed(() => currentWinnerName.value);
   
   return {
     getState,
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,57 @@
+import type { ComputedRef } from 'vue';
+export interface Players {
+  value: Player[],
+}
+
 export interface Player {
   id: number,
   name: string,
   color: string,
   score: number,
+}
+
+export interface UsePlayers {
+  players: Players;
+  getPlayers: ComputedRef<Player[]>;
+  setScore: (id: number, score: number) => void;
+  addPlayer: (player: Player) => void;
+  setPlayers: (newPlayers: Player[]) => void;
+  editPlayer: (player: Player) => void;
+  resetScores: () => void;
+  removePlayer: (id: number) => void;
+  removePlayers: () => void;
+  getPlayerById: (id: number) => Player | undefined;
+  addPlayerPoints: (id: number, points: number) => void;
+}
+
+export interface UseLocaLstorage {
+  savePlayers: (players: Player[]) => void;
+  getPlayers: () => Player[];
+  saveWinners: (winners: Player[]) => void;
+  getWinners: () => Player[];
+  saveState: (state: string) => void;
+  getState: () => string;
+  saveCurrentPlayerId: (id: number) => void;
+  getCurrentPlayerId: () => number;
+  saveCurrentTurn: (turn: number) => void;
+  getCurrentTurn: () => number;
+  saveCurrentWinnerName: (name: string) => void;
+  getCurrentWinnerName: () => string;
+}
+
+export interface UseGame {
+  getState: ComputedRef<string>;
+  setState: (state: string) => void;
+  getCurrentTurn: ComputedRef<number>;
+  setCurrentTurn: (turn: number) => void;
+  getCurrentPlayerId: ComputedRef<number>;
+  setCurrentPlayerId: (id: number) => void;
+  getCurrentWinnerName: ComputedRef<string>;
+  setCurrentWinnerName: (name: string) => void;
+  addWinner: (winner: Player) => void;
+  getWinners: ComputedRef<Player[]>;
+  resetGame: () => void;
+  getDarts: ComputedRef<number>;
+  setDarts: (darts: number) => void;
+  changePlayerToNext: () => void;
 }
