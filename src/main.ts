f5da3fcde345ed2cda5603187c3ba20db7388d45
Adrian <adrian.korzan@gmail.com>
2023-02-19 01:45:20 +0100

    [21] deploy

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
-
 const vuetify = createVuetify({
   components,
   directives,
+  // @ts-ignore
   icons: {
     defaultSet: 'mdi',
-  }
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
